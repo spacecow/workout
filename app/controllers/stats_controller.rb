69a6094921dd39b4ef5006036a2f5cae53dec1cd
Johan Sveholm <jsveholm@gmail.com>
2012-09-19 10:21:11 +0900

    charts for 30 days

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,13 @@
 class StatsController < ApplicationController
   def charts
     @selected = :charts
+
     @durations7 = Topentry.where('duration = ? and category = ?', 7, 'duration').order('days.date').includes(:day).group_by{|e| e.user.userid}
     @distances7 = Topentry.where('duration = ? and category = ?', 7, 'distance').order('days.date').includes(:day).group_by{|e| e.user.userid}
+
+    @durations30 = Topentry.where('duration = ? and category = ?', 30, 'duration').order('days.date').includes(:day).group_by{|e| e.user.userid}
+    @distances30 = Topentry.where('duration = ? and category = ?', 30, 'distance').order('days.date').includes(:day).group_by{|e| e.user.userid}
+
     @weights = CurrentState.order('days.date').includes(:day).group_by{|e| e.user.userid}
   end
 
--- lib/tasks/generate_missing_topentries.rake
+++ lib/tasks/generate_missing_topentries.rake
@@ -1,3 +1,4 @@
 task :generate_missing_topentries => :environment do
   Topentry.generate_total_missing_entries(7)
+  Topentry.generate_total_missing_entries(30)
 end
